5c73c65f0fa45efe332f528acccbf2ab7371d5e1
XinyuZhang <xzhang755@gatech.edu>
2018-01-11 00:46:35 -0500

    Added registration form

--- src/Main.js
+++ src/Main.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { WrappedRegister } from './Register';
 
 export class Main extends React.Component {
     render() {
         return (
-            <div>this is main</div>
+            <section className="main">
+                <WrappedRegister/>
+            </section>
         );
     }
 }
