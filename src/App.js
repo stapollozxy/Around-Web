18cad74aeffef9cbec4005efbf92c988f68d78c1
XinyuZhang <xzhang755@gatech.edu>
2018-01-11 00:19:28 -0500

    Added Main.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Header } from './Header';
-
+import { Main } from './Main';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <Header/>
+        <Main />
       </div>
     );
   }
