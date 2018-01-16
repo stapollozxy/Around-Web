44a03a72dd0387e6bd79cf59779af06a7cab1ed1
XinyuZhang <xzhang755@gatech.edu>
2018-01-15 22:46:46 -0500

    added Loging form UI

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { WrappedRegister } from './Register';
-
+import {Login} from './Login';
 export class Main extends React.Component {
     render() {
         return (
-            <section className="main">
-                <WrappedRegister/>
-            </section>
+            <div className="main">
+                <Login/>
+            </div>
         );
     }
 }
