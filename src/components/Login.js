6b8237eabe301b513d2edb9338efc4f13d291c1b
XinyuZhang <xzhang755@gatech.edu>
2018-01-15 23:55:21 -0500

    Added login Ajax

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,9 @@
 import React from 'react'
-
-import { Form, Icon, Input, Button, Checkbox } from 'antd';
+import $ from 'jquery'
+import { API_ROOT } from '../constants.js'
+import { Form, Icon, Input, Button } from 'antd';
 import {Link } from 'react-router-dom'
+import {message} from 'antd/lib/index'
 const FormItem = Form.Item;
 
 class NormalLoginForm extends React.Component {
@@ -10,6 +12,20 @@ class NormalLoginForm extends React.Component {
         this.props.form.validateFields((err, values) => {
             if (!err) {
                 console.log('Received values of form: ', values);
+                $.ajax({
+                    url: `${API_ROOT}/login`,
+                    method: 'POST',
+                    data: JSON.stringify({
+                        username: values.username,
+                        password: values.password,
+                    })
+                }).then((response) => {
+                    message.success(response);
+                }, (error) => {
+                    message.error(error.responseText);
+                }).catch((error) => {
+                    message.error(error);
+                });
             }
         });
     }
@@ -18,7 +34,7 @@ class NormalLoginForm extends React.Component {
         return (
             <Form onSubmit={this.handleSubmit} className="login-form">
                 <FormItem>
-                    {getFieldDecorator('userName', {
+                    {getFieldDecorator('username', {
                         rules: [{ required: true, message: 'Please input your username!' }],
                     })(
                         <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
