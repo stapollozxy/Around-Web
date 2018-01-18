3cab39f65773426f8671eb0a2070fbbd212b7fe4
XinyuZhang <xzhang755@gatech.edu>
2018-01-17 22:24:02 -0500

    Added Home style

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import { Tabs, Button } from 'antd';
+
+
+
+const TabPane = Tabs.TabPane;
+const operations= <Button>Extra Action</Button>;
+
 export class Home extends React.Component {
     render() {
         return (
-        <div> this is home</div>
+            <Tabs tabBarExtraContent={operations} className="main-tabs">
+                <TabPane tab="Posts" key="1">Tab 1</TabPane>
+                <TabPane tab="Map" disabled key="2">Tab 2</TabPane>
+            </Tabs>
         )
     }
 }
