6e4dec117f82d5eb04961b282968ee9bf8f469b4
Xinyu Zhang <xiaoyuapollo@gmail.com>
2018-01-17 23:33:07 -0500

    loading geolocation state finished

---
+++ src/components/Gallery.js
@@ -0,0 +1,10 @@
+import React, { Component }from 'react';
+import PropTypes from 'prop-types';
+import GridGallery from 'react-grid-gallery';
+
+export class Gallery extends Component {
+
+    render() {
+
+    }
+}
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Tabs, Button } from 'antd';
+import { Tabs, Button, Spin } from 'antd';
 import {GEO_OPTIONS} from '../constants';
 
 
@@ -7,7 +7,13 @@ const TabPane = Tabs.TabPane;
 const operations= <Button>Extra Action</Button>;
 
 export class Home extends React.Component {
+    state = {
+        loadingGeoLocation : true,
+        error: '',
+    }
+
     componentDidMount() {
+        this.setState({loadingGeoLocation:true});
         this.getGeoLocation();
     }
 
@@ -22,21 +28,31 @@ export class Home extends React.Component {
             );
         } else {
             /* geolocation IS NOT available */
+            this.setState({ error: 'Your browser does not support geolocation!'});
         }
     }
 
     onSuccessLoadGeoLocation = (position) => {
         console.log(position);
+        this.setState({loadingGeoLocation:false, error: ''});
     }
     onFailedLoadGeoLocation = () => {
+        this.setState({loadingGeoLocation:false, error: 'Your browser does not support geolocation!'});
 
     }
+    getGalleryPanelContent = () => {
+        if (this.state.error) {
+            return <div>{this.state.error}</div>;
+        } else if (this.state.loadingGeoLocation) {
+            return <Spin tip="Loading..."/>
+        }
+    }
     render() {
-
-
         return (
             <Tabs tabBarExtraContent={operations} className="main-tabs">
-                <TabPane tab="Posts" key="1">Tab 1</TabPane>
+                <TabPane tab="Posts" key="1">
+                    {this.getGalleryPanelContent()}
+                </TabPane>
                 <TabPane tab="Map"  key="2">Tab 2</TabPane>
             </Tabs>
         )
