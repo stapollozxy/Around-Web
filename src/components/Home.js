bef7cf536d9f1477355bd7978deb2e180ee3258f
XinyuZhang <xzhang755@gatech.edu>
2018-01-17 22:43:38 -0500

    Added GeoLocation

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,43 @@
 import React from 'react';
 import { Tabs, Button } from 'antd';
-
+import {GEO_OPTIONS} from '../constants';
 
 
 const TabPane = Tabs.TabPane;
 const operations= <Button>Extra Action</Button>;
 
 export class Home extends React.Component {
+    componentDidMount() {
+        this.getGeoLocation();
+    }
+
+
+    getGeoLocation = () => {
+        if ("geolocation" in navigator) {
+            /* geolocation is available */
+            navigator.geolocation.getCurrentPosition(
+                this.onSuccessLoadGeoLocation,
+                this.onFailedLoadGeoLocation,
+                GEO_OPTIONS,
+            );
+        } else {
+            /* geolocation IS NOT available */
+        }
+    }
+
+    onSuccessLoadGeoLocation = (position) => {
+        console.log(position);
+    }
+    onFailedLoadGeoLocation = () => {
+
+    }
     render() {
+
+
         return (
             <Tabs tabBarExtraContent={operations} className="main-tabs">
                 <TabPane tab="Posts" key="1">Tab 1</TabPane>
-                <TabPane tab="Map" disabled key="2">Tab 2</TabPane>
+                <TabPane tab="Map"  key="2">Tab 2</TabPane>
             </Tabs>
         )
     }
