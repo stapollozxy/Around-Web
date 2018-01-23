db3c69a1351824b985f85c1990a6cafbc98e0d01
Xinyu Zhang <xiaoyuapollo@gmail.com>
2018-01-23 01:45:53 -0500

    fixed back

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,83 +1,85 @@
-import React, { Component } from 'react';
-import $ from 'jquery';
+import React from 'react';
 import { Tabs, Spin } from 'antd';
-import { Gallery } from './Gallery';
-//import { WrappedAroundMap } from './AroundMap';
-import { CreatePostButton } from './CreatePostButton';
-import { API_ROOT, TOKEN_KEY, AUTH_PREFIX, GEO_OPTIONS, POS_KEY } from '../constants';
+import $ from 'jquery';
+import {GEO_OPTIONS, POS_KEY, API_ROOT, AUTH_PREFIX, TOKEN_KEY} from '../constants';
+import {Gallery} from './Gallery';
+import {CreatePostButton} from './CreatePostButton'
+
+const TabPane = Tabs.TabPane;
 
 
-export class Home extends Component {
+export class Home extends React.Component {
     state = {
-        posts: [],
-        loadingGeoLocation: false,
+        loadingGeoLocation : true,
         loadingPosts: false,
         error: '',
+        posts: [],
     }
 
-    onTabChange = (key) => {
-        console.log(key);
+    componentDidMount() {
+        this.setState({loadingGeoLocation:true});
+        this.getGeoLocation();
     }
+
 
     getGeoLocation = () => {
-        if (navigator && navigator.geolocation) {
-            this.setState({ loadingGeoLocation: true, error: '' });
+        if ("geolocation" in navigator) {
+            /* geolocation is available */
             navigator.geolocation.getCurrentPosition(
                 this.onSuccessLoadGeoLocation,
                 this.onFailedLoadGeoLocation,
-                GEO_OPTIONS
+                GEO_OPTIONS,
             );
         } else {
+            /* geolocation IS NOT available */
             this.setState({ error: 'Your browser does not support geolocation!'});
         }
     }
 
     onSuccessLoadGeoLocation = (position) => {
-        this.setState({ loadingGeoLocation: false, error: '' });
+        console.log(position);
+        this.setState({loadingGeoLocation:false, error: ''});
         const { latitude: lat, longitude: lon } = position.coords;
         localStorage.setItem(POS_KEY, JSON.stringify({ lat: lat, lon: lon }));
         this.loadNearbyPosts();
-    }
-
-    onFailedLoadGeoLocation = () => {
-        this.setState({ loadingGeoLocation: false, error: 'Failed to load geo location!' });
-    }
-
-    componentDidMount() {
-        this.getGeoLocation();
     }
 
     loadNearbyPosts = () => {
         const { lat, lon } = JSON.parse(localStorage.getItem(POS_KEY));
         //const lat = 37.7915953;
         //const lon = -122.3937977;
-        this.setState({ loadingPosts: true });
+        this.setState({ loadingPosts: true, error: '' });
         return $.ajax({
+            // url format: root/search?lat=&lon=&range=20
             url: `${API_ROOT}/search?lat=${lat}&lon=${lon}&range=20`,
             method: 'GET',
             headers: {
-                'Authorization': `${AUTH_PREFIX} ${localStorage.getItem(TOKEN_KEY)}`,
+                Authorization: `${AUTH_PREFIX} ${localStorage.getItem(TOKEN_KEY)}`,
             },
-        }).then((response) => {
-            this.setState({ posts: response, error: '' });
-            console.log(response);
-        }, (error) => {
-            this.setState({ error: error.responseText });
-        }).then(() => {
-            this.setState({ loadingPosts: false });
-        }).catch((error) => {
+        }).then( (response) => {
+                this.setState({ loadingPosts:false, posts: response, error: '' });
+                console.log(response);
+            }, (error) => {
+                this.setState({ loadingPosts:error, error: error.responseText });
+                console.log(error);
+            }
+        ).catch((error) => {
             console.log(error);
         });
     }
 
+    onFailedLoadGeoLocation = () => {
+        this.setState({loadingGeoLocation:false, error: 'Your browser does not support geolocation!'});
+
+    }
     getGalleryPanelContent = () => {
         if (this.state.error) {
             return <div>{this.state.error}</div>
         } else if (this.state.loadingGeoLocation) {
-            return <Spin tip="Loading geo location ..."/>
+            return <Spin tip="Loading..."/>
         } else if (this.state.loadingPosts) {
             return <Spin tip="Loading posts ..."/>
-        } else if (this.state.posts) {
+        } else if (this.state.posts && this.state.posts.length > 0) {
             const images = this.state.posts.map((post) => {
                 return {
                     user: post.user,
@@ -86,51 +88,22 @@ export class Home extends Component {
                     thumbnailWidth: 400,
                     thumbnailHeight: 300,
                     caption: post.message,
-                };
-            });
-            // const images = IMAGES_TEST.map(url => ({
-            //   src: url,
-            //   thumbnail: url,
-            //   thumbnailWidth: 400,
-            //   thumbnailHeight: 300,
-            //   caption: 'This is the best place in the world! @richard',
-            // }));
 
-            return (
-                <Gallery
-                    images={images}
-                />
-            );
+                }
+            });
+            return <Gallery images={images}/>
         }
-        return null;
     }
 
     render() {
-        const TabPane = Tabs.TabPane;
         const createPostButton = <CreatePostButton loadNearbyPosts={this.loadNearbyPosts}/>;
         return (
-            <Tabs
-                defaultActiveKey="1"
-                onChange={this.onTabChange}
-                tabBarExtraContent={createPostButton}
-                className="main-tabs"
-            >
+            <Tabs tabBarExtraContent={createPostButton} className="main-tabs">
                 <TabPane tab="Posts" key="1">
                     {this.getGalleryPanelContent()}
                 </TabPane>
-                <TabPane tab="Map" key="2">
-                    {/*<WrappedAroundMap
-                        posts={this.state.posts}
-                        loadNearbyPosts={this.loadNearbyPosts}
-                        googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyC4R6AN7SmujjPUIGKdyao2Kqitzr1kiRg&v=3.exp&libraries=geometry,drawing,places"
-                        loadingElement={<div style={{ height: `100%` }} />}
-                        containerElement={<div style={{ height: `600px` }} />}
-                        mapElement={<div style={{ height: `100%` }} />}
-                    />*/}
-                </TabPane>
+                <TabPane tab="Map"  key="2">Tab 2</TabPane>
             </Tabs>
-        );
-    }
+        )
+    };
 }
-
-
