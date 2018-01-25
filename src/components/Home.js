887bb26a7c1229743b3bbbff14ec90c4a9677ae2
Xinyu Zhang <xiaoyuapollo@gmail.com>
2018-01-24 23:16:58 -0500

    Marker first added

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,7 @@ import $ from 'jquery';
 import {GEO_OPTIONS, POS_KEY, API_ROOT, AUTH_PREFIX, TOKEN_KEY} from '../constants';
 import {Gallery} from './Gallery';
 import {CreatePostButton} from './CreatePostButton'
+import { WrappedAroundMap } from './AroundMap';
 
 const TabPane = Tabs.TabPane;
 
@@ -102,7 +103,16 @@ export class Home extends React.Component {
                 <TabPane tab="Posts" key="1">
                     {this.getGalleryPanelContent()}
                 </TabPane>
-                <TabPane tab="Map"  key="2">Tab 2</TabPane>
+                <TabPane tab="Map"  key="2">Tab 2
+                    <WrappedAroundMap
+                        loadNearbyPosts={this.loadNearbyPosts}
+                        posts={this.state.posts}
+                        googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyC4R6AN7SmujjPUIGKdyao2Kqitzr1kiRg&v=3.exp&libraries=geometry,drawing,places"
+                        loadingElement={<div style={{ height: `100%` }} />}
+                        containerElement={<div style={{ height: `600px` }} />}
+                        mapElement={<div style={{ height: `100%` }} />}
+                    />
+                </TabPane>
             </Tabs>
         )
     };
